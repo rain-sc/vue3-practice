1b58ebf3c68ba9fab32d3117881d4769119301ca
DESKTOP-A319T96\Rain <shunchitrain@gmail.com>
2024-05-31 15:09:42 +0800

    add login afther redirect dashboard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,54 @@
 import type { RouteRecordRaw } from 'vue-router'
 import { createRouter, createWebHashHistory } from 'vue-router'
 
+export const Layout = () => import('@/layout/index.vue')
+
 export const routes: RouteRecordRaw[] = [
+  {
+    path: '/redirect',
+    component: Layout,
+    meta: { hidden: true },
+    children: [
+      {
+        path: '/redirect/:path(.*)',
+        component: () => import('@/views/redirect/index.vue'),
+      },
+    ],
+  },
   {
     path: '/login',
     component: () => import('@/views/login/index.vue'),
     meta: { hidden: true },
+  },
+  {
+    path: '/',
+    name: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/views/dashboard/index.vue'),
+        name: 'Dashboard',
+        meta: {
+          title: 'dashboard',
+          icon: 'homepage',
+          affix: true,
+          keepAlive: true,
+          alwaysShow: false,
+        },
+      },
+      {
+        path: '401',
+        component: () => import('@/views/error-page/401/index.vue'),
+        meta: { hidden: true },
+      },
+      {
+        path: '404',
+        component: () => import('@/views/error-page/404/index.vue'),
+        meta: { hidden: true },
+      },
+    ],
   },
 ]
 
---
+++ src/views/error-page/401/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    401
+  </div>
+</template>
+
+<style scoped>
+
+</style>
