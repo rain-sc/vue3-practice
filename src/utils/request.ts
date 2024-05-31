2331b1b5b2a9e35a8e276b916e0d6a89557a6371
DESKTOP-A319T96\Rain <shunchitrain@gmail.com>
2024-05-31 15:25:56 +0800

    alter axios response error message

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,6 +2,12 @@ import type { AxiosResponse, InternalAxiosRequestConfig } from 'axios'
 import axios from 'axios'
 import { ElMessage, ElMessageBox } from 'element-plus'
 import TOKEN_KEY from '@/enums/CaheEnum'
+import { useUserStoreHook } from '@/store/modules/user'
+
+function logout() {
+  useUserStoreHook().resetToken()
+  window.location.reload()
+}
 
 const service = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_API,
@@ -24,28 +30,16 @@ service.interceptors.request.use(
 service.interceptors.response.use(
   (response: AxiosResponse) => {
     return response
-    // const res = response.data
-
-    // if (res.code !== 20000) {
-    //   ElMessage({
-    //     message: res.message || 'Error',
-    //     type: 'error',
-    //     duration: 5 * 1000,
-    //   })
-    //   return Promise.reject(new Error(res.message || 'Error'))
-    // }
-    // if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-    //   ElMessageBox.confirm(`You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout'`, {
-    //     confirmButtonText: 'Re-Login',
-    //     cancelButtonText: 'Cancel',
-    //     type: 'warning',
-    //   }).then(() => {
-
-    //   })
-    // }
-    // return Promise.reject(new Error(res.message || 'Error'))
   },
   (error: any) => {
+    if (error.response.status === 401) {
+      ElMessageBox.confirm(`You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout'`, {
+        confirmButtonText: 'Re-Login',
+        cancelButtonText: 'Cancel',
+        type: 'warning',
+      })
+      logout()
+    }
     ElMessage({
       message: error.message,
       type: 'error',
