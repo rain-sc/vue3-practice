36959252ba0ad6a795836f9182c39aa8f0c8e82d
Rain <shunchitrain@gmail.com>
2024-06-08 19:14:57 +0800

    alter get department list change to useHook method

---
+++ src/hooks/useDepartmentList.ts
@@ -0,0 +1,40 @@
+import { getDepartmentListAPI } from '@/api/department'
+import type { DepartmentListType } from '@/api/department/types'
+
+function useDepartmentList() {
+  const departmentList = ref<DepartmentListType[]>([])
+  const loading = ref<boolean>(false)
+
+  function transListToTreeData(list: DepartmentListType[], parentId: string | number): DepartmentListType[] {
+    return list
+      .filter(item => item.pid === parentId)
+      .map(item => ({
+        ...item,
+        children: transListToTreeData(list, item.id!),
+      }))
+  }
+
+  async function getDepartmentList() {
+    loading.value = true
+
+    try {
+      const res = await getDepartmentListAPI()
+      const resData = res.data.data!
+      departmentList.value = transListToTreeData(resData, 0)
+    }
+    catch (error) {
+      console.error(error)
+    }
+    finally {
+      loading.value = false
+    }
+  }
+
+  return {
+    getDepartmentList,
+    departmentList,
+    loading,
+  }
+}
+
+export default useDepartmentList
